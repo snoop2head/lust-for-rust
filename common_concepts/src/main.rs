aa897f9c7ebd202ac2e167a87cd459abe9826dbe
noopy <young_ahn@yonsei.ac.kr>
2020-07-04 00:37:24 +0900

    update mutability concept

--- common_concepts/src/main.rs
+++ common_concepts/src/main.rs
@@ -1,1 +1,8 @@
-// https://doc.rust-lang.org/book/ch03-00-common-programming-concepts.html
+// https://doc.rust-lang.org/book/ch03-01-variables-and-mutability.html
+
+fn main() {
+    let mut x = 5;
+    println!("The value of x is {}",x);
+    x = 6;
+    println!("Mutated value of x is {}",x);
+}
