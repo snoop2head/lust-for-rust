5f6232c66423ff596047e4af4a565bce2730aec5
noopy <young_ahn@yonsei.ac.kr>
2020-07-03 23:46:16 +0900

    add looping

--- 2_guessing_game/src/main.rs
+++ 2_guessing_game/src/main.rs
@@ -7,25 +7,31 @@ use rand::Rng;
 
 // entrypoint
 fn main() {
+    println!("Guess the number between 1 and 100");
+
     let secret_number = rand::thread_rng().gen_range(1,101);
 
-    println!("Guess the number!");
-    println!("Please input your guess.");
-    
-    // storing user input as mutable variable
-    let mut guess = String::new(); // String type, associated with new() function
+    loop {
+        println!("Please input your guess.");
 
-    io::stdin()
-        .read_line(&mut guess) // handle input from the user with read_line(), access to mutable variable guess
-        .expect("Failed to read line"); // handling potential failure
+        // storing user input as mutable variable
+        let mut guess = String::new(); // String type, associated with new() function
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        io::stdin()
+            .read_line(&mut guess) // handle input from the user with read_line(), access to mutable variable guess
+            .expect("Failed to read line"); // handling potential failure
 
-    println!("You guessed: {}", guess);
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue, // continue looping when non-number value is given as input
+        };
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too Big!"),
-        Ordering::Equal => println!("You got it right!"),
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too Big!"),
+            Ordering::Equal => println!("You got it right!"),
+        }
     }
 }
